f0b0526862e580990fa4571210ee987334ee2b05
Paul Serby <paul.serby@clock.co.uk>
2011-07-09 16:52:51 +0100

    Adding unit test for entity-definition

---
+++ test/entity-definition.test.js
@@ -0,0 +1,59 @@
+var
+	EntityDefinition = require('../../piton-entity').EntityDefinition,
+	assert = require('assert');
+
+
+function createTestEntityDefinition() {
+	var entityDefinition = new EntityDefinition();
+		entityDefinition.schema = {
+			name: {
+			},
+			age: {
+			},
+			active: {
+				defaultValue: true
+			}
+		};
+	return entityDefinition;
+}
+
+module.exports = {
+	'makeBlank returns correct obejct': function() {
+		var entityDefinition = createTestEntityDefinition();
+		assert.eql({
+			name: null,
+			age: null,
+			active: null
+		}, entityDefinition.makeBlank());
+	},
+	'makeBlank without a customer schema creates a empty object': function() {
+		var entityDefinition = new EntityDefinition();
+		assert.eql({}, entityDefinition.makeBlank());
+	},
+	'makeDefault without a customer schema creates a empty object': function() {
+		var entityDefinition = new EntityDefinition();
+		assert.eql({}, entityDefinition.makeDefault());
+	},
+	'makeDefault returns correct obejct': function() {
+		var entityDefinition = createTestEntityDefinition();
+		assert.eql({
+			name: null,
+			age: null,
+			active: true
+		}, entityDefinition.makeDefault());
+	},
+	'makeDefault extends passed object correctly': function() {
+		var entityDefinition = createTestEntityDefinition();
+		assert.eql({
+			name: 'Paul',
+			age: null,
+			active: true
+		}, entityDefinition.makeDefault({ name: 'Paul' }));
+	},
+	'stripUnknownProperties strips out extra properties': function() {
+		var entityDefinition = createTestEntityDefinition();
+		assert.eql({
+			name: 'Paul'
+		}, entityDefinition.stripUnknownProperties({ name: 'Paul', extra: 'This should not be here' }));
+	}
+};
